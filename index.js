84fd79870731f03159dd84d48a938d2c6a6696e5
zekiunal <Bun@2081>
2020-01-17 23:02:08 +0300

    .

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import React from 'react';
 import {createStackNavigator} from 'react-navigation-stack';
 import SignInScreen from "./src/screens/SignInScreen";
 import SignUpScreen from "./src/screens/SignUpScreen";
+import InitializeScreen from './src/screens/InitializeScreen'
 import {Provider, Context} from './src/context/AuthContext';
 import AuthFlowConfig from "./src/helpers/AuthFlowConfig";
 
 const createAuthFlowNavigation = () => {
     return createStackNavigator({
+        Initialize: InitializeScreen,
         SignIn: SignInScreen,
         SignUp: SignUpScreen
     });
---
+++ src/screens/InitializeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+const InitializeScreen = () => {
+    return (
+        <Text>This is InitializeScreen</Text>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default InitializeScreen;
