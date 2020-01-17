c0a2e638163598f72f95cc0886f27dfb8110657e
Zeki Unal <zekiunal@gmail.com>
2020-01-17 16:01:50 +0300

    .

--- src/helpers/AuthFlowConfig.js
+++ src/helpers/AuthFlowConfig.js
@@ -5,23 +5,45 @@ export default class AuthFlowConfig {
         api: {},
         SignIn: {},
         SignUp: {},
-        SignOut: {}
+        SignOut: {},
+        translate: {
+            en: {
+                RTL: false,
+                button: {
+                    signUp: 'Sign Up',
+                    signIn: 'Sign In',
+                    signOut: 'Sign Out'
+                }
+            },
+            tr: {
+                RTL: false,
+                button: {
+                    signUp: 'Üye Ol',
+                    signIn: 'Giriş',
+                    signOut: 'Çıkış'
+                }
+            }
+        },
+        language: 'en'
     };
 
     static setConfig(config) {
         console.log('setConfig');
-
-        this.configuration = config;
+        this.configuration = {...this.configuration, config}
     }
 
     static getConfig() {
         console.log('getConfig');
-
         return this.configuration;
     }
 
     static getApiConfig() {
         console.log('getApiConfig');
+        return this.configuration.api;
+    }
+
+    static setApiConfig(config) {
+        this.configuration.api = {...this.configuration.api, config}
         return this.configuration.api;
     }
 }
